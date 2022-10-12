416447464b5aaa2c21fc0cba26508a9abf894d44
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-10-12 22:51:52 +0200

    🎉 Initialized git repository

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+<!-- ~/src/routes/index.svelte -->
+
+<script>
+    import * as prismicH from '@prismicio/helpers';
+    export let data;
+  </script>
+  
+  <main>
+    <div class="container">
+      <h1>Homepage</h1>
+      <pre>{JSON.stringify(data.document, null, 2)}</pre>
+    </div>
+  </main>
+
+  
+  
+  <style>
+    main {
+      font-family: sans-serif;
+    }
+  
+    .container {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      padding: 0px 20px;
+    }
+  
+    .container > * {
+      width: 100%;
+      max-width: 700px;
+    }
+  </style>
+  
