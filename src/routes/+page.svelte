cee7035988a1ef8e89bfb9f3409d7f3518f0e66d
M4TThys123 <70703948+M4TThys123@users.noreply.github.com>
2022-10-14 13:05:30 +0200

    🚑️ Made critical hotfixes:changed page.js to +page.js, moved the lib folder out of the routes folder & made a vendors folder

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,32 @@
 <!-- ~/src/routes/index.svelte -->
-
 <script>
-    import * as prismicH from '@prismicio/helpers';
-    export let data;
-  </script>
-  
-  <main>
-    <div class="container">
-      <h1>Homepage</h1>
-      <pre>{JSON.stringify(data.document, null, 2)}</pre>
-    </div>
-  </main>
+  import * as prismicH from "@prismicio/helpers";
+  export let data;
 
-  
-  
-  <style>
-    main {
-      font-family: sans-serif;
-    }
-  
-    .container {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 0px 20px;
-    }
-  
-    .container > * {
-      width: 100%;
-      max-width: 700px;
-    }
-  </style>
-  
+  console.log(data.documents);
+</script>
+
+<main>
+  <div class="container">
+    <h1>Homepage</h1>
+    <pre>{JSON.stringify(data.documents, null, 2)}</pre>
+  </div>
+</main>
+
+<style>
+  main {
+    font-family: sans-serif;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px 20px;
+  }
+
+  .container > * {
+    width: 100%;
+    max-width: 700px;
+  }
+</style>
